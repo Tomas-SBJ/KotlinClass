b7da8504503ebaa3386ef46f898dec84516d6c27
tomassbj <tomassbj@gmail.com>
2024-11-04 22:00:58 -0300

    resolved error

--- src/main/kotlin/br/com/alura/gameapi/principal/BaseTest.kt
+++ src/main/kotlin/br/com/alura/gameapi/principal/BaseTest.kt
@@ -2,14 +2,9 @@ package br.com.alura.gameapi.principal
 
 import br.com.alura.gameapi.data.Base
 import br.com.alura.gameapi.data.GamesDAO
-import br.com.alura.gameapi.models.Game
 
 fun main() {
     val manager = Base.getEntityManager()
     val gamesDAO = GamesDAO(manager)
-
-    val gameList: List<Game> = gamesDAO.getGames()
-    println(gameList)
-
     manager.close()
 }
